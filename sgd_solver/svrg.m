2a900b8f459db4a4119b3ac1fb6265689e9afa97
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-04-27 17:25:02 -0400

    d
    d

--- sgd_solver/svrg.m
+++ sgd_solver/svrg.m
@@ -123,7 +123,7 @@ function [w, infos] = svrg(problem, in_options)
     if optgap < options.tol_optgap
         fprintf('Optimality gap tolerance reached: tol_optgap = %g\n', options.tol_optgap);
     elseif epoch == options.max_epoch
-        fprintf('Max epoch reached: max_epochr = %g\n', options.max_epoch);
+        %fprintf('Max epoch reached: max_epochr = %g\n', options.max_epoch);
     end
     
     infos.subinfos = subinfos;    
